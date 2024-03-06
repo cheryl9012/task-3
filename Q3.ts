361d42aaddc744c06dfcb9b1bf32686bce9a3cae
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-06 21:23:28 +0500

    Add files via upload
    It's the third question of the assignment.

---
+++ Q3.ts
@@ -0,0 +1,7 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personame: string = "Eric";
+
+console.log(personame.toLowerCase());
+console.log(personame.toUpperCase());
+console.log(personame.charAt(0).toUpperCase()+personame.slice(5));
